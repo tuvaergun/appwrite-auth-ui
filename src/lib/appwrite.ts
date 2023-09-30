24bff7a5936dd0c5a441ded099a1430622fd2247
Yasin Ergün <tuvaergun@gmail.com>
2023-09-30 16:28:54 +0300

    fix

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,38 +1,51 @@
 import { Account, Client, Databases, Storage, type Models } from 'appwrite';
 
 export type AppwritePage = {
-	domain: string,
-	name?: string,
-	successUrl: string,
-	failureUrl: string,
-	provider: string,
-	providerData: string,
-	borderRadius: 'xs' | 'm' | 'xl',
-	brandColor: 'primary' | 'success' | 'information' | 'warning' | 'neutral' | string,
-	allowGuest: boolean,
-	allowMagicUrl: boolean,
-	allowPhone: boolean,
-	allowGoogle: boolean,
-	allowGitHub: boolean,
-	allowTwitter: boolean,
-	allowFacebook: boolean,
-	fileId: string,
-	privacyPolicy?: string,
-	termsOfService?: string,
-	allowSignUp: boolean
+	domain: string;
+	name?: string;
+	successUrl: string;
+	failureUrl: string;
+	provider: string;
+	providerData: string;
+	borderRadius: 'xs' | 'm' | 'xl';
+	brandColor: 'primary' | 'success' | 'information' | 'warning' | 'neutral' | string;
+	allowGuest: boolean;
+	allowMagicUrl: boolean;
+	allowPhone: boolean;
+	allowGoogle: boolean;
+	allowGitHub: boolean;
+	allowTwitter: boolean;
+	allowFacebook: boolean;
+	fileId: string;
+	privacyPolicy?: string;
+	termsOfService?: string;
+	allowSignUp: boolean;
 } & Models.Document;
 
 export const AppwriteClient = new Client()
-	.setEndpoint('https://appwrite.authui.site/v1')
-	.setProject('authUi');
+	.setEndpoint('https://bilpp.cloud/v1')
+	.setProject('651820dfa4194d96b596');
 
 export const AppwriteDatabases = new Databases(AppwriteClient);
 export const AppwriteAccount = new Account(AppwriteClient);
 export const AppwriteStorage = new Storage(AppwriteClient);
 
-
 export const AppwriteService = {
 	getLogo: (imageId: string) => {
-		return AppwriteStorage.getFilePreview('logos', imageId, undefined, 128, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, 'webp').toString();
+		return AppwriteStorage.getFilePreview(
+			'logos',
+			imageId,
+			undefined,
+			128,
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			'webp'
+		).toString();
 	}
-}
+};
